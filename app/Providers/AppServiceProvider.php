b6337e528e366bc5641801e93eca128331e63df7
Clement Jebakumar <cjebakumar@vmware.com>
2022-05-08 10:10:17 +0530

    Commit changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+    //
     }
 
     /**
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (request()->header('x-forwarded-proto') == 'https' || request()->header('x-forwarded-scheme') == 'https' || request()->header('x-scheme') == 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
